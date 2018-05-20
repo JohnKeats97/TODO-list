cb5754005322b6dbd477574c06c6b7a74491fe7c
Unknown <johnbuevich@yahoo.com>
2018-05-20 17:58:38 +0300

    addButtonBox function

--- Data/Modules/Controllers/ToDoController.js
+++ Data/Modules/Controllers/ToDoController.js
@@ -91,37 +91,38 @@ class ToDoController
         this.buttonListener();
     }
 
+    addButtonBox(button) {
+        button.style.cssText =  "box-shadow: 0 0 0 2px #cfdbe5d1;" +
+            "background-color: #3195B6;";
+    }
+
     buttonListener() {
         let _add = document.querySelectorAll(".button-add")[0];
         _add.addEventListener('click', function () {
             this.showAdd();
             let button = document.querySelectorAll(".button-add")[0];
-            button.style.cssText =  "box-shadow: 0 0 0 2px #cfdbe5d1;" +
-                                    "background-color: #3195B6;";
+            this.addButtonBox(button);
         }.bind(this));
 
         let _all = document.querySelectorAll(".button-all")[0];
         _all.addEventListener('click', function () {
             this.showToDo();
             let button = document.querySelectorAll(".button-all")[0];
-            button.style.cssText =  "box-shadow: 0 0 0 2px #cfdbe5d1;" +
-                                    "background-color: #3195B6;";
+            this.addButtonBox(button);
         }.bind(this));
 
         let _active = document.querySelectorAll(".button-active")[0];
         _active.addEventListener('click', function () {
             this.showToDo(this.active);
             let button = document.querySelectorAll(".button-active")[0];
-            button.style.cssText =  "box-shadow: 0 0 0 2px #cfdbe5d1;" +
-                                    "background-color: #3195B6;";
+            this.addButtonBox(button);
         }.bind(this));
 
         let _completed = document.querySelectorAll(".button-completed")[0];
         _completed.addEventListener('click', function () {
             this.showToDo(this.complited);
             let button = document.querySelectorAll(".button-completed")[0];
-            button.style.cssText =  "box-shadow: 0 0 0 2px #cfdbe5d1;" +
-                                    "background-color: #3195B6;";
+            this.addButtonBox(button);
         }.bind(this));
     }
 
